b4ef84a1d0d9acbdd3c84bfa9155803f42fbe023
hedgehogs-mind <peter@hedgehogs-mind.com>
2020-06-08 22:20:46 +0200

    .

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from time import sleep
-
 import cv2
 import time
-import math
-import numpy as np
 
-from f135 import straighten_35mm_negative, get_35mm_strip_colors, get_35mm_strip_top_border_coords, \
-    get_35mm_strip_bottom_border_coords
-from strip import create_bordered_negative, create_bw_negative, get_sprocket_holes_contours, split_sprocket_holes, \
-    get_average_sprocket_hole_size
-from util import draw_line, group_contours_by_distance, closest_transitive_contours, contours_top_line, \
-    contours_bottom_line, contours_center_line, n_closest_contours, line_angle, most_left_contour, most_right_contour, \
-    contour_center, points_to_line, get_k_colors, sort_colors_by_brightness, calc_white_balance_diff
-
-# todo: what happens if i have a negative with background all around?
+from util.image import add_border, blur, bw
 
 window = 'negative'
 cv2.namedWindow(window, cv2.WINDOW_NORMAL)
 cv2.resizeWindow(window, 800, 600)
-
-
-
 
 
 #negative = cv2.imread("images/test_negative_small_rotated_mirrored.tiff")
@@ -31,84 +16,41 @@ cv2.resizeWindow(window, 800, 600)
 
 dir = "images/ektar_16/"
 #negative = cv2.imread(dir + "ektar_16bit_01_s.tif", cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR)
-negative = cv2.imread(dir + "ektar_16bit_01_r.tif", cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR)
-
+negative = cv2.imread(dir + "ektar_16bit_02_s.tif", cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR)
 
 
 # Processing start
 t_start = time.time()
 
-rotated_negative = straighten_35mm_negative(negative)
+# First step: extract plain strip
+bordered = add_border(negative, (255, 255, 255))
+blurred = blur(bordered)
+bw_img = bw(blurred, mode=cv2.THRESH_BINARY_INV)
 
 
-
-# Compute colors for white balance
-darkest_color, brightest_color = get_35mm_strip_colors(rotated_negative)
-
-
-
-
-
-
-# Let us do the white balance :)
-wb_negative = rotated_negative.copy()
-
-color_mask = calc_white_balance_diff(brightest_color)
-color_correction = -color_mask
-
-print("Mask: {}".format(color_mask))
-print("Diff: {}".format(color_correction))
-
-
-print("Darkest color: {}".format(darkest_color))
-print("Brightest color: {}".format(brightest_color))
-
-# Apply color correction
-
-before_type = wb_negative.dtype
-wb_negative = wb_negative.astype(dtype=np.int64)
-wb_negative[:, :] += color_correction
-wb_negative = np.clip(wb_negative, 0, np.iinfo(before_type).max)
-wb_negative = wb_negative.astype(dtype=before_type)
-
-
-# And invert
-wb_negative = cv2.bitwise_not(wb_negative)
-
-# Now let us handle the contrast
-max_val = np.iinfo(wb_negative.dtype).max
-pos_brightest_color = max_val-(darkest_color+color_correction)
-pos_darkest_color = max_val-(brightest_color+color_correction)
-
-color_displacement = np.mean(pos_darkest_color) * 1.15
-color_factor = max_val/(np.mean(pos_brightest_color)-color_displacement) * 0.7
-
-print("Pos. brightest: {}".format(pos_brightest_color))
-print("Pos. darkest:   {}".format(pos_darkest_color))
-
-print("Displacement: {}".format(color_displacement))
-print("Factor: {}".format(color_factor))
-
-before_type = wb_negative.dtype
-wb_negative = wb_negative.astype(dtype=np.int64)
-
-# First displacement
-wb_negative[:, :] = (wb_negative[:, :]-color_displacement)
-wb_negative = np.clip(wb_negative, 0, np.iinfo(before_type).max)
-
-# Then stretch
-wb_negative[:, :] = wb_negative[:, :] * color_factor
-wb_negative = np.clip(wb_negative, 0, np.iinfo(before_type).max)
-
-wb_negative = wb_negative.astype(dtype=before_type)
 
 t_end = time.time()
 print("time: {:.3f}s".format((t_end-t_start)))
 # Processing end
 
 
-cv2.imshow(window, wb_negative)
-cv2.waitKey(0)
+(cnts, _) = cv2.findContours(bw_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnt = cnts[0]
 
+for i in range(0, 501):
+
+    img = bordered.copy()
+
+    epsilon = i/10
+    poly = cv2.approxPolyDP(cnt, epsilon, True)
+
+    cv2.drawContours(img, [poly], -1, (255, 0, 255), 6)
+    print("epsilon: {}, #pts: {}".format(epsilon, len(poly)))
+
+    cv2.imshow(window, img)
+    cv2.waitKey(10)
+
+
+cv2.waitKey(-1)
 cv2.destroyAllWindows()
 
---
+++ util/image.py
@@ -0,0 +1,86 @@
+import math
+
+import cv2
+import numpy as np
+
+
+def add_border(image, color, percentage=0.01, minimum=1):
+    """
+    Adds a border to the given image. Supports 8bit and 16bit images.
+
+    The border with is relative to the width or height of the image,
+    the greatest of those two is selected and multiplied by the given
+    percentage. If the resulting width is lower than the given minimum
+    value, the latter is used instead.
+
+    :param image: Image to add border to.
+    :param color: Tuple of border color (BGR). Values only in range 0-255.
+    :param percentage: Border width relative to the biggest image length.
+    :return: Original image with border around it. Image will be bigger than the original.
+    """
+    assert minimum >= 1, "Min border width must be > 0"
+    assert percentage >= 0, "Only positive percentage is allowed"
+
+    border_color = np.asarray(color)
+
+    if image.dtype == np.uint16:
+        border_color = border_color.astype(np.uint32)
+        shifted = border_color + 1
+        stretched = shifted * 256
+        border_color = stretched - 1  # normalized again
+
+    border_color = (
+        int(border_color[0]),
+        int(border_color[1]),
+        int(border_color[2])
+    )
+
+    (h, w) = image.shape[:2]
+    max_dim = max(h, w)
+    border_with = int(max(minimum, math.ceil(max_dim * percentage)))
+
+    return cv2.copyMakeBorder(
+        image,
+        border_with, border_with, border_with, border_with,
+        cv2.BORDER_CONSTANT,
+        value=border_color
+    )
+
+
+def blur(image, percentage=0.008, minimum=1):
+    """
+    Blurs the given image relative to its size. The value of width or height is used
+    (the greater one) and multiplied by the given percentage. Is the resulting value than
+    the minimum value
+    :param image: Image to blur.
+    :param percentage: Blur size relative to the biggest image dimension.
+    :param minimum: Minimum blur size (>= 1).
+    :return: Blurred image.
+    """
+
+    assert percentage >= 0, "Percentage must be >= 0"
+    assert minimum >= 1, "Min must be at least 1"
+
+    (h, w) = image.shape[:2]
+    max_dim = max(h, w)
+    blur_size = int(max(minimum, math.ceil(max_dim * percentage)))
+
+    return cv2.blur(image, (blur_size, blur_size))
+
+
+def bw(image, threshold=0.9, mode=cv2.THRESH_BINARY):
+    """
+    Turns an image into a black and white image (bi color/binary).
+
+    :param image: Must be BGR format. Image to be turned bi color black/white.
+    :param threshold: Threshold value relative to image max value (makes it bit depth independent).
+    :param mode: Threshold mode used for cv2.threshold function.
+    :return: 8bit Black and white image. White parts stay white, everything lower than white*percentage is black.
+    """
+    max_value = np.iinfo(image.dtype).max
+    threshold_value = int(threshold * max_value)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    (_, bw_img) = cv2.threshold(gray, threshold_value, max_value, mode)
+
+    return bw_img.astype(np.uint8)
